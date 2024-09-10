e2f727e3f3e00ed8dc1452ba0937d943ae08f49f
storezhang <storezhang@gmail.com>
2024-09-10 13:42:13 +0800

    feat(加载器): 增加支持类型

--- changer.go
+++ changer.go
@@ -1,7 +1,11 @@
 package config
 
+import (
+	"github.com/pangum/config/internal/runtime"
+)
+
 // Changer 配置修改
 type Changer interface {
 	// Changed 当配置发生修改时的回调方法
-	Changed(key string, original string, changed string)
+	Changed(original runtime.Pointer, changed runtime.Pointer)
 }
--- loader.go
+++ loader.go
@@ -6,6 +6,9 @@ import (
 
 // Loader 加载器
 type Loader interface {
+	// Supports 支持的类型
+	Supports() []string
+
 	// Load 加载配置时调用
 	Load(config runtime.Pointer) (err error)
 }
