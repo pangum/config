b0de0b28c9045bf4bf34c2e9bbfd9d1b992be15a
storezhang <storezhang@gmail.com>
2024-09-10 14:11:41 +0800

    feat(加载器): 增加上下文

--- loader.go
+++ loader.go
@@ -1,14 +1,13 @@
 package config
 
 import (
+	"context"
+
 	"github.com/pangum/config/internal/runtime"
 )
 
 // Loader 加载器
 type Loader interface {
-	// Supports 支持的类型
-	Supports() []string
-
 	// Load 加载配置时调用
-	Load(config runtime.Pointer) (err error)
+	Load(ctx context.Context, config runtime.Pointer) (err error)
 }
