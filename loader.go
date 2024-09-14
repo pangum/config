3193f1fb6e6b1f0848177ec30b6317ca63723252
storezhang <storezhang@gmail.com>
2024-09-14 16:35:26 +0800

    refactor(加载器): 泛化配置类型，为后续对key和value进行处理做准备

--- loader.go
+++ loader.go
@@ -11,5 +11,5 @@ type Loader interface {
 
 	// Load 加载配置时调用
 	// !不使用原始配置类型是为了方便后续处理，不能随意改变方法签名中配置的类型
-	Load(ctx context.Context, target map[string]any) (loaded bool, err error)
+	Load(ctx context.Context, target *map[string]any) (loaded bool, err error)
 }
