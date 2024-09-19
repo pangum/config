0a1ef9f0f9039cd7ff183073c54c4d94d88778d8
storezhang <storezhang@gmail.com>
2024-09-19 09:40:21 +0800

    refactor(加载器): 增加支持的配置文件格式

--- loader.go
+++ loader.go
@@ -9,6 +9,9 @@ type Loader interface {
 	// Local 是否是本地加载器
 	Local() bool
 
+	// Supports 支持的文件格式
+	Supports() []string
+
 	// Load 加载配置时调用
 	// !不使用原始配置类型是为了方便后续处理，不能随意改变方法签名中配置的类型
 	Load(ctx context.Context, target *map[string]any) (loaded bool, err error)
